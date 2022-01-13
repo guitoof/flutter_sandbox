825dcd214af5cd7277bd6c9be4f9293e8687d68d
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-01-13 18:49:48 +0100

    :sparkles: (Webview) Add webview page
    This should enable us to test the gesture handling behavior with webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sandbox/global_gestures_listener_plugin/global_gesture_listener_with_simple_gesture_detector.dart';
+import 'package:flutter_sandbox/global_gestures_listener_plugin/global_gesture_listener_with_simple_listener.dart';
 import 'package:flutter_sandbox/global_gestures_listener_plugin/global_gesture_listener_with_stack.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,27 +30,40 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: GlobalGestureListenerWithStack(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(widget.title),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text("Here is the result of the stuff you've done:"),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-              ],
+      home: GlobalGestureListnerWithSimpleListener(
+        child: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
+              bottom: const TabBar(tabs: [
+                Tab(icon: Icon(Icons.add_circle_outline_rounded)),
+                Tab(icon: Icon(Icons.web)),
+              ]),
             ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: _doStuff,
-            tooltip: 'Do stuff...',
-            child: Icon(Icons.science),
+            body:
+                TabBarView(physics: NeverScrollableScrollPhysics(), children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Here is the result of the stuff you've done:"),
+                    Text(
+                      '$_counter',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ],
+                ),
+              ),
+              WebView(
+                initialUrl: 'https://flutter.dev/',
+              ),
+            ]),
+            floatingActionButton: FloatingActionButton(
+              onPressed: _doStuff,
+              tooltip: 'Do stuff...',
+              child: Icon(Icons.science),
+            ),
           ),
         ),
       ),
