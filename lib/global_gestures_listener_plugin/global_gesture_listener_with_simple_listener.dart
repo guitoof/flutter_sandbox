940713d0305888ecf9b3a860502c52e40f78d724
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-01-13 19:07:44 +0100

    :construction: (Listener) Handle all kind of PointerEvents

--- lib/global_gestures_listener_plugin/global_gesture_listener_with_simple_listener.dart
+++ lib/global_gestures_listener_plugin/global_gesture_listener_with_simple_listener.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class GlobalGestureListnerWithSimpleListener extends StatelessWidget {
@@ -12,6 +13,26 @@ class GlobalGestureListnerWithSimpleListener extends StatelessWidget {
       onPointerUp: (PointerUpEvent event) {
         print(
             '🔴 Global Gesture Listener Plugin DETECTED POINTER UP ############################');
+      },
+      onPointerDown: (PointerDownEvent event) {
+        print(
+            '🔴 Global Gesture Listener Plugin DETECTED POINTER DOWN ############################');
+      },
+      onPointerMove: (PointerMoveEvent event) {
+        print(
+            '🔴 Global Gesture Listener Plugin DETECTED POINTER MOVE ############################');
+      },
+      onPointerHover: (PointerHoverEvent event) {
+        print(
+            '🔴 Global Gesture Listener Plugin DETECTED POINTER HOVER ############################');
+      },
+      onPointerSignal: (PointerSignalEvent event) {
+        print(
+            '🔴 Global Gesture Listener Plugin DETECTED POINTER SIGNAL ############################');
+      },
+      onPointerCancel: (PointerCancelEvent event) {
+        print(
+            '🔴 Global Gesture Listener Plugin DETECTED POINTER CANCEL ############################');
       },
       // behavior: HitTestBehavior.translucent,
       child: child,
