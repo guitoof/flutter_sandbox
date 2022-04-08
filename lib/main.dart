c09fcaab3bb598dc4ef8876717e1f833951d362e
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-04-08 10:34:44 +0200

    :construction: (main) Display DataTable & PaginatedDataTable on home

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,38 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+// The "soruce" of the table
+class MyData extends DataTableSource {
+  // Generate some made-up data
+  final List<Map<String, dynamic>> _data = List.generate(
+      200,
+      (index) => {
+            "id": index,
+            "title": "Item $index",
+            "price": Random().nextInt(10000),
+            "toto": 'TOTO',
+          });
+
+  @override
+  bool get isRowCountApproximate => false;
+  @override
+  int get rowCount => _data.length;
+  @override
+  int get selectedRowCount => 0;
+  @override
+  DataRow getRow(int index) {
+    return DataRow(cells: [
+      DataCell(Text(_data[index]['id'].toString())),
+      DataCell(Text(_data[index]["title"])),
+      DataCell(Text(_data[index]["price"].toString())),
+      DataCell(Text(_data[index]["toto"])),
+    ]);
+  }
 }
 
 class MyApp extends StatefulWidget {
@@ -12,14 +43,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _counter = 0;
-
-  void _doStuff() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,21 +52,50 @@ class _MyAppState extends State<MyApp> {
           title: Text(widget.title),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Here is the result of the stuff you've done:"),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                DataTable(
+                  headingRowColor: MaterialStateProperty.resolveWith((states) {
+                    if (states.contains(MaterialState.selected)) {
+                      return Colors.red;
+                    }
+                    return Colors.blue;
+                  }),
+                  columns: [
+                    DataColumn(label: Text('Data')),
+                    DataColumn(label: Text('Table')),
+                  ],
+                  rows: [
+                    DataRow(cells: [
+                      DataCell(Text('Hello')),
+                      DataCell(Text('World')),
+                    ]),
+                    DataRow(cells: [
+                      DataCell(Text('Hello')),
+                      DataCell(Text('Dash')),
+                    ]),
+                  ],
+                ),
+                PaginatedDataTable(
+                  headingRowColor: MaterialStateProperty.resolveWith((states) {
+                    if (states.contains(MaterialState.selected)) {
+                      return Colors.red;
+                    }
+                    return Colors.blue;
+                  }),
+                  rowsPerPage: 8,
+                  columns: [
+                    DataColumn(label: Text('Paginated')),
+                    DataColumn(label: Text('Data')),
+                    DataColumn(label: Text('Table')),
+                    DataColumn(label: Text('Toto')),
+                  ],
+                  source: MyData(),
+                ),
+              ],
+            ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _doStuff,
-          tooltip: 'Do stuff...',
-          child: Icon(Icons.science),
         ),
       ),
     );
