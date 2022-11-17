6b3cdfb1d29f87cd11319d094fc78232d7c42bc1
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-11-17 10:02:38 +0100

    :construction: (Buttons) DIsplay 2 buttons

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _counter = 0;
-
-  void _doStuff() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,21 +21,20 @@ class _MyAppState extends State<MyApp> {
           title: Text(widget.title),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Here is the result of the stuff you've done:"),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [1, 2]
+                .map((item) => ElevatedButton(
+                    style: TextButton.styleFrom(minimumSize: Size(150, 150)),
+                    onPressed: () {
+                      print('Click on: $item');
+                    },
+                    child: Text(
+                      item.toString(),
+                      style: TextStyle(fontSize: 32),
+                    )))
+                .toList(),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _doStuff,
-          tooltip: 'Do stuff...',
-          child: Icon(Icons.science),
         ),
       ),
     );
